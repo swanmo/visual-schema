4094ce9795f427e21999a764c359d56ec3ffceac
swanmo <swanmo@gmail.com>
2014-03-19 23:41:13 +0100

    Cleanup.

--- src/standardRenderer.js
+++ src/standardRenderer.js
@@ -1,6 +1,5 @@
 define(['jquery'], function($) {
-	var arrOfColors = ["#efefef", "#e7e7e7", "#dfdfdf", "#d7d7d7", "#d0d0d0", "#c7c7c7"];
-	var colorPos = 0;
+
 	return {
 		render: function(roots) {
 			var rootElem = $('<div>');
@@ -36,95 +35,77 @@ define(['jquery'], function($) {
 			var $childContainer;
 			var $div1 = $('<div>');
 
-				var desc;
-				var css;
-				if (entry.name=="element") {
-					desc = "<div class='e'>E</div> " + entry.attrs.name;
-					css = "element";
-					$div1.addClass("padded");
-					if (entry.attrs.minOccurs == "0") {
-						css += " optional";
-					}
-				} else if (entry.name=="sequence") {
-					desc = "<div class='s' title='sequence'>...</div> ";
-				} else if (entry.name=="complexContent") {
-					return parent;
-				} else if (entry.name=="extension") {
-					desc = "<span class='ctBadge'>extension base " + entry.attrs.base + "</span>";
-					addWrapperContainer = true;
-				} else if (entry.name=="complexType") {
-					if (entry.attrs.name) {
-						desc = "<span class='ctBadge' title='complexType'>" + entry.attrs.name + "</span> ";	
-					} else {
-						desc = "<span class='ctBadge'>complexType</span> ";
-					}
-
-					addWrapperContainer = true;
-				} else {
-					if (!entry.attrs) {
-						console.log("no attrs");
-					}
-					if (entry.attrs.name) {
-						desc = entry.attrs.name + ' (' + entry.name + ')';
-					} else {
-						desc = entry.name;
-					}
+			var desc;
+			var css;
+			if (entry.name=="element") {
+				desc = "<div class='e'>E</div> " + entry.attrs.name;
+				css = "element";
+				$div1.addClass("padded");
+				if (entry.attrs.minOccurs == "0") {
+					css += " optional";
 				}
-
-				var title = "";
-				if (entry.nsMap) {
- 					for (var key in entry.nsMap) {
- 						title += key + " = " + entry.nsMap[key] + "\n \n";
- 					}
-				}
-
-				$childContainer = $('<div>');
-
-				
-				var $span = $('<span>');
-				$div1.append($span);
-				$span.attr("title", title);
-				$div1.append($childContainer);
-
-				if (hasBranching) {
-					$span.html(desc);
-					$childContainer.addClass('hasBranch');
-				} else {
-					$span.html(desc);
-				}
-				$span.addClass(css);
-
-				if (addWrapperContainer) {
-					$wrapperDiv = $('<div>');
-					$wrapperDiv.appendTo(parent);
-					if (isExtensionBase) {
-						console.log("marking xb")
-						$wrapperDiv.addClass('xb');
-					} else {
-						$wrapperDiv.addClass('ctContainer')	;
-					}
-					
-					$div1.appendTo($wrapperDiv);	
-				} else {
-					$div1.appendTo(parent);	
-				}
-
-			/*else if (entry.name=="element") {
-				var $div = $('<div>');
-				$div.append($('<span>').text(entry.attrs.name + " (" + entry.attrs.type + ")"));
-				entries.push($div);
 			} else if (entry.name=="sequence") {
-				var $div = $('<div>');
-				$div.text('...'));
-				entries.push($div);
+				desc = "<div class='s' title='sequence'>...</div> ";
 			} else if (entry.name=="complexContent") {
-				var $div1 = $('<div>');
-				$div1.append($('<span>').text('CC'));
-				entries.push($div1);
+				return parent;
 			} else if (entry.name=="extension") {
-				listEntry.addClass("eSequence");
-				listEntry.append($('<span title="extension">').text("X").addClass("icon"));
-			}*/
+				desc = "<span class='ctBadge'>extension base " + entry.attrs.base + "</span>";
+				addWrapperContainer = true;
+			} else if (entry.name=="complexType") {
+				if (entry.attrs.name) {
+					desc = "<span class='ctBadge' title='complexType'>" + entry.attrs.name + "</span> ";	
+				} else {
+					desc = "<span class='ctBadge'>complexType</span> ";
+				}
+
+				addWrapperContainer = true;
+			} else {
+				if (!entry.attrs) {
+					console.log("no attrs");
+				}
+				if (entry.attrs.name) {
+					desc = entry.attrs.name + ' (' + entry.name + ')';
+				} else {
+					desc = entry.name;
+				}
+			}
+
+			var title = "";
+			if (entry.nsMap) {
+					for (var key in entry.nsMap) {
+						title += key + " = " + entry.nsMap[key] + "\n \n";
+					}
+			}
+
+			$childContainer = $('<div>');
+
+			var $span = $('<span>');
+			$div1.append($span);
+			$span.attr("title", title);
+			$div1.append($childContainer);
+
+			if (hasBranching) {
+				$span.html(desc);
+				$childContainer.addClass('hasBranch');
+			} else {
+				$span.html(desc);
+			}
+			$span.addClass(css);
+
+			if (addWrapperContainer) {
+				$wrapperDiv = $('<div>');
+				$wrapperDiv.appendTo(parent);
+				if (isExtensionBase) {
+					console.log("marking xb")
+					$wrapperDiv.addClass('xb');
+				} else {
+					$wrapperDiv.addClass('ctContainer')	;
+				}
+				
+				$div1.appendTo($wrapperDiv);	
+			} else {
+				$div1.appendTo(parent);	
+			}
 
 			return $childContainer;
 		},
