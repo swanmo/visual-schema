99c83761e1e8b3d9e79ca989742ebfc94de7f528
swanmo <swanmo@gmail.com>
2014-12-08 23:10:33 +0100

    Sökväg till jquery.

--- Gruntfile.js
+++ Gruntfile.js
@@ -25,7 +25,7 @@ module.exports = function (grunt) {
           include: ['main'],
          
           paths: {
-            jquery: '../lib/jquery'
+            jquery: '../lib/static/jquery'
           }
         }
       }
