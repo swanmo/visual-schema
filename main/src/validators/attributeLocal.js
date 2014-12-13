22bd5a4c875fe442e6ac1818b07f903c59a2d0e5
swanmo <swanmo@gmail.com>
2014-12-13 19:48:17 +0100

    Attribut-validering av attributeLocal

--- main/src/validators/attributeLocal.js
+++ main/src/validators/attributeLocal.js
@@ -1,15 +1,27 @@
 /**
- * https://www.evolveum.com/downloads/midpoint/1.9/midpoint-xsddocs-1.9/schemas/XMLSchema_xsd/elements/schema.html
+ * https://www.evolveum.com/downloads/midpoint/1.9/midpoint-xsddocs-1.9/schemas/XMLSchema_xsd/elements/attribute_1.html
  */
 define(['validators/util'], function (util) {
+    var u = util.getInstance();
     var contentElements = [
       'annotation',
       'simpleType'
      ];
 
+     var attrs = [
+      {n:'default', t:'string'},
+      {n:'fixed', t:'string'},
+      {n:'form', t:'formChoice'},
+      {n:'id'},
+      {n:'name', t:'NCName'},
+      {n:'ref', t:'QName'},
+      {n:'type', t:'QName'},
+      {n:'use', t:'prohibited|optional|required'}
+     ];
+
     return {
         getValidationErrors: function(schemaElement) {
-            return util.getInstance().validateChildren(schemaElement, contentElements);
+            return u.errorsOnly(u.validateAll(schemaElement, contentElements, attrs));
         }
     };
 });
