080e7445db744cd767d9799fa58ae9a77715ec3e
swanmo <swanmo@gmail.com>
2014-12-14 00:37:19 +0100

    Validering av attribut på include

--- main/src/validators/include.js
+++ main/src/validators/include.js
@@ -1,11 +1,15 @@
 define(['validators/util'], function (util) {
     var contentElements = [
       'annotation'
+    ];
+    var attrs = [
+      {n:'id', t:'ID'},
+      {n:'schemaLocation', u:'r'}
      ];
 
     return {
         getValidationErrors: function(schemaElement) {
-            return util.getInstance().validateChildren(schemaElement, contentElements);
+        	return util.getInstance().validateAll(schemaElement, contentElements, attrs);
         }
     };
 });
