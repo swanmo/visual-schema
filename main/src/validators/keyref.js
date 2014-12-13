50e721f9baf41e6ff427ada5d7f3d396d86f0d47
swanmo <swanmo@gmail.com>
2014-12-14 00:41:00 +0100

    Validering av attribut för keyref

--- main/src/validators/keyref.js
+++ main/src/validators/keyref.js
@@ -1,14 +1,20 @@
 // TODO https://www.evolveum.com/downloads/midpoint/1.9/midpoint-xsddocs-1.9/schemas/XMLSchema_xsd/elements/extension.html
 define(['validators/util'], function (util) {
-    var contentElements = [
-      'annotation',
-      'field',
-      'selector'
-     ];
+  var contentElements = [
+    'annotation',
+    'field',
+    'selector'
+  ];
 
-    return {
-        getValidationErrors: function(schemaElement) {
-            return util.getInstance().validateChildren(schemaElement, contentElements);
-        }
-    };
+  var attrs = [
+    {n:'id', t:'ID'},
+    {n:'name', t:'NCName', u:'r'},
+    {n:'refer', t:'QName', u:'r'}
+  ];
+
+  return {
+    getValidationErrors: function(schemaElement) {
+      return u.errorsOnly(u.validateAll(schemaElement, contentElements, attrs));
+    }
+  };
 });
