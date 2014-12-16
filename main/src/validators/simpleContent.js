f36e76b298d7399d773b8b4d0de2f77e923cba7e
swanmo <swanmo@gmail.com>
2014-12-16 23:02:14 +0100

    Validering av element simpleContent.

--- main/src/validators/simpleContent.js
+++ main/src/validators/simpleContent.js
@@ -1,7 +1,5 @@
 /**
  * https://www.evolveum.com/downloads/midpoint/1.9/midpoint-xsddocs-1.9/schemas/XMLSchema_xsd/elements/simpleContent.html
- * @param  {[type]} util [description]
- * @return {[type]}      [description]
  */
 define(['validators/util'], function (util) {
     var contentElements = [
@@ -10,9 +8,13 @@ define(['validators/util'], function (util) {
       'restriction'
      ];
 
+     var attrs = [
+      {n:'id', t:'ID'}
+    ];
+
     return {
         getValidationErrors: function(schemaElement) {
-            return util.getInstance().validateChildren(schemaElement, contentElements);
+            return util.getInstance().validateAll(schemaElement, contentElements, attrs);
         }
     };
 });
