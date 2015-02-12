60ec610f2b7c85f5481657e77dff98a4f1ef4d24
swanmo <swanmo@gmail.com>
2015-02-12 22:09:12 +0100

    Formattering

--- main/src/datePresentationUtil.js
+++ main/src/datePresentationUtil.js
@@ -1,26 +1,22 @@
 define([], function() {
-	var mothNames = [
-		'Jan',
-		'Feb',
-		'Mar',
-		'Apr',
-		'May',
-		'Jun',
-		'Jul',
-		'Aug',
-		'Sep',
-		'Oct',
-		'Nov',
-		'Dec'
-	];
-	
-	function nameOfMonth(monthIndex) {
-		return mothNames[monthIndex];
-	}
-
-	function approx(_date) {
-		var now = new Date();
-	}
+  var mothNames = [
+    'Jan',
+    'Feb',
+    'Mar',
+    'Apr',
+    'May',
+    'Jun',
+    'Jul',
+    'Aug',
+    'Sep',
+    'Oct',
+    'Nov',
+    'Dec'
+  ];
+  
+  function nameOfMonth(monthIndex) {
+    return mothNames[monthIndex];
+  }
 
   function asText(_no) {
     switch(_no) {
@@ -49,52 +45,50 @@ define([], function() {
     }
   }
 
-	return {
-		month: function(_date) {
-			return nameOfMonth(_date.getUTCMonth());
-		},
-		since: function (_date, now) {
-			now = now || new Date();
-			
+  return {
+    month: function(_date) {
+      return nameOfMonth(_date.getUTCMonth());
+    },
+    since: function (_date, now) {
+      now = now || new Date();
 
       var seconds = Math.floor((now - _date) / 1000);
       var interval = Math.floor(seconds / 31536000);
-      // console.log('since', seconds, Math.floor(seconds / 86400));
       
       if (interval === 1) {
-        return "A year ago";
+        return 'A year ago';
       } else if (interval > 1) {
-        return asText(interval) + " years ago";
+        return asText(interval) + ' years ago';
       }
       interval = Math.floor(seconds / 2592000);
       if (interval === 1) {
-        return "A month ago";
+        return 'A month ago';
       } else if (interval > 1) {
-        return asText(interval) + " months ago";
+        return asText(interval) + ' months ago';
       }
       interval = Math.floor(seconds / 86400);
       if (interval === 1) {
-        return "A day ago";
+        return 'A day ago';
       } else if (interval > 6 && interval < 9) {
-        return "A week ago";
+        return 'A week ago';
       } else if (interval > 13 && interval < 17) {
-        return "Two weeks ago";
+        return 'Two weeks ago';
       } else if (interval > 20 && interval < 23) {
-        return "Three weeks ago";
+        return 'Three weeks ago';
       } else if (interval > 1) {
-        return asText(interval) + " days ago";
+        return asText(interval) + ' days ago';
       }
       interval = Math.floor(seconds / 3600);
       if (interval === 1) {
-        return "An hour ago";
+        return 'An hour ago';
       } else if (interval > 1) {
-        return asText(interval) + " hours ago";
+        return asText(interval) + ' hours ago';
       }
       interval = Math.floor(seconds / 60);
       if (interval === 1) {
-        return "A minute ago";
+        return 'A minute ago';
       } else if (interval > 1) {
-        return asText(interval) + " minutes ago";
+        return asText(interval) + ' minutes ago';
       }
       interval = Math.floor(seconds);
       if (interval < 5) {
@@ -102,8 +96,8 @@ define([], function() {
       } else if (interval < 30) {
         return 'A few seconds ago';
       } else {
-        return interval + ' seconds';
+        return interval + ' seconds ago';
       }
-		}
-	};
+    }
+  };
 });
